04d8c102f19cbd775c8ee6bee88877d5f4b39aca
Кисляк Игорь <Kislyakim@gmail.com>
2024-12-09 01:11:41 +0500

    Открывается и закрывается

--- js/functions.js
+++ js/functions.js
@@ -2,68 +2,59 @@
 /* eslint-disable prefer-template */
 /* eslint-disable eol-last */
 
-export function lenStrCheck(str,maxLen){
-  str = str.replaceAll(' ','');
+export function lenStrCheck(str, maxLen) {
+  str = str.replaceAll(' ', '');
   return str.length <= maxLen;
 }
-lenStrCheck('asdfghj',7);
+
+lenStrCheck('asdfghj', 7);
+
 /*
 Функция, которая принимает время начала и конца раб дня,
 время старта и продолжительность встречи в мин.
 возвращает true, если встреча не выходит за рамки раб дня и false, если выходит.
 */
-function workDayLimit(dayStart, dayEnd, meetStart, meetTime){
+function workDayLimit(dayStart, dayEnd, meetStart, meetTime) {
   let meetEnd = '';
-  switch (meetStart.length){
-    case (5):
-    case (4 && meetEnd.indexOf(':') === 2):
-      meetEnd = String(+meetStart.slice(0,2) + Math.floor(meetTime / 60)) +
-      ':' + String(+meetStart.slice(3) + meetTime % 60) ;
 
-      if (meetEnd.indexOf(':') === 1){ (meetEnd = '0' + meetEnd);
-      }
+  // Функция для добавления ведущих нулей к времени
+  function addLeadingZero(time) {
+    return time.indexOf(':') === 1 ? '0' + time : time;
+  }
 
-      if (+meetEnd.slice(3) >= 60){ meetEnd = String(+meetEnd.slice(0,2) + 1) + ':' + String (+meetEnd.slice(3) % 60);
-      }
+  switch (meetStart.length) {
+    case 5:
+    case (4 && meetStart.indexOf(':') === 2):
+      meetEnd = String(+meetStart.slice(0, 2) + Math.floor(meetTime / 60)) + ':' + String(+meetStart.slice(3) + meetTime % 60);
       break;
-    case (4 && meetEnd.indexOf(':') === 1):
-    case (3):
-      meetEnd = String(+meetStart.slice(0,1) + Math.floor(meetTime / 60)) +
-      ':' + String(+meetStart.slice(2) + meetTime % 60) ;
 
-      if (meetEnd.indexOf(':') === 1){ (meetEnd = '0' + meetEnd);
-      }
-
-      if (+meetEnd.slice(3) >= 60){ meetEnd = String(+meetEnd.slice(0,2) + 1) + ':' + String (+meetEnd.slice(3) % 60);
-      }
+    case 4:
+    case 3:
+      meetEnd = String(+meetStart.slice(0, 1) + Math.floor(meetTime / 60)) + ':' + String(+meetStart.slice(2) + meetTime % 60);
       break;
   }
 
-  if (dayStart.indexOf(':') === 1){ dayStart = '0' + dayStart;
-  }
+  // Убедимся, что meetEnd правильно отформатировано
+  meetEnd = addLeadingZero(meetEnd);
 
-  if (meetStart.indexOf(':') === 1){ meetStart = '0' + meetStart;
-  }
+  // Форматируем время начала рабочего дня и встречи
+  dayStart = addLeadingZero(dayStart);
+  meetStart = addLeadingZero(meetStart);
+  dayEnd = addLeadingZero(dayEnd);
 
-  if (dayStart.slice(0,2) > meetStart.slice(0,2)){ return false;
-  }
-  else if ((dayStart.slice(0,2) === meetStart.slice(0,2)) && dayStart.slice(3) > meetStart.slice(3)){ return false;
-  }
+  // Проверяем, что встреча начинается после рабочего дня
+  if (dayStart.slice(0, 2) > meetStart.slice(0, 2)) return false;
+  if (dayStart.slice(0, 2) === meetStart.slice(0, 2) && dayStart.slice(3) > meetStart.slice(3)) return false;
 
-  if (dayEnd.indexOf(':') === 1){ dayEnd = '0' + dayEnd;
-  }
+  // Проверяем, что встреча заканчивается до конца рабочего дня
+  if (dayEnd.slice(0, 2) > meetEnd.slice(0, 2)) return true;
+  if (dayEnd.slice(0, 2) === meetEnd.slice(0, 2) && dayEnd.slice(3) >= meetEnd.slice(3)) return true;
 
-  if (dayEnd.slice(0,2) > meetEnd.slice(0,2)){ return true;
-  }
-  else if ((dayEnd.slice(0,2) === meetEnd.slice(0,2)) && dayEnd.slice(3) >= meetEnd.slice(3)){ return true;
-  } else { return false;
-  }
-
+  return false;
 }
-workDayLimit('08:00', '17:30', '14:00', 90);
-workDayLimit('8:0', '10:0', '8:0', 120);
-workDayLimit('08:00', '14:30', '14:00', 90);
-workDayLimit('14:00', '17:30', '08:0', 90);
-workDayLimit('8:00', '17:30', '08:00', 900);
 
-
+console.log(workDayLimit('08:00', '17:30', '14:00', 90));  // true
+console.log(workDayLimit('8:0', '10:0', '8:0', 120));     // false
+console.log(workDayLimit('08:00', '14:30', '14:00', 90));  // false
+console.log(workDayLimit('14:00', '17:30', '08:0', 90));  // true
+console.log(workDayLimit('8:00', '17:30', '08:00', 900)); // false
